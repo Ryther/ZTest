798da23a930fe3fc9bd3e82a37634867b4ef0a74
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-06-03 17:45:22 +0200

    [ClientMain.java]Pulito codice

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -1,14 +1,13 @@
 package client;
 
 import chatUtils.data.ChatMessage;
-import java.io.ByteArrayOutputStream;
+import chatUtils.data.Consts;
 import java.io.IOException;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import utils.data.DataHandler;
 
 /**
  *
@@ -18,7 +17,7 @@ public class ClientMain {
 
     public static void main(String[] args) throws IOException, InterruptedException {
  
-        InetSocketAddress crunchifyAddr = new InetSocketAddress("localhost", 1111);
+        InetSocketAddress crunchifyAddr = new InetSocketAddress(Consts.INETADDRESS, Consts.PORT);
         SocketChannel crunchifyClient = SocketChannel.open(crunchifyAddr);
 
         log("Connecting to Server on port 1111...");
@@ -32,60 +31,22 @@ public class ClientMain {
         companyDetails.add("Google");
         companyDetails.add("Crunchify");
 
-        ChatMessage chatMessage = new ChatMessage("test");
-//        ObjectOutputStream objectOutputStream = new ObjectOutputStream(Channels.newOutputStream(crunchifyClient));
+        ChatMessage chatMessage = new ChatMessage("Ryther");
+
         for (String companyName : companyDetails) {
 
             chatMessage.setMessage(companyName);
             chatMessage.setDateTime();
-            byte[] message;
-//            Integer messageBufferSize = chatMessage.toString().getBytes().length;
-//            byte[] messageLength = messageBufferSize.toString().getBytes();
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ObjectOutput oOut = null;
-            try {
-                oOut = new ObjectOutputStream(baos);
-                oOut.writeObject(chatMessage);
-                message = baos.toByteArray();
-            } finally {
-                oOut.close();
-                baos.close();
-            }
-//            byte[] message = chatMessage.toString().getBytes();
-            ByteBuffer buffer = ByteBuffer.wrap(message);
+            ByteBuffer buffer = DataHandler.objectToByteBuffer(chatMessage);
             crunchifyClient.write(buffer);
-//            buffer = ByteBuffer.wrap(message);
-//            crunchifyClient.write(buffer);
-            
-            
-//            messageBufferSize = chatMessage.getMessage().getBytes().length;
-//            messageLength = messageBufferSize.toString().getBytes();
-//            message = chatMessage.getMessage().getBytes();
-//            buffer = ByteBuffer.wrap(messageLength);
-//            crunchifyClient.write(buffer);
-//            buffer = ByteBuffer.wrap(message);
-//            crunchifyClient.write(buffer);
-//            
-//            
-//            messageBufferSize = chatMessage.getDateTime().getBytes().length;
-//            messageLength = messageBufferSize.toString().getBytes();
-//            message = chatMessage.getDateTime().getBytes();
-//            buffer = ByteBuffer.wrap(messageLength);
-//            crunchifyClient.write(buffer);
-//            buffer = ByteBuffer.wrap(message);
-//            crunchifyClient.write(buffer);
-//            objectOutputStream.writeObject(chatMessage);
+            buffer.clear();
             
             log("sending: " + companyName);
-            buffer.clear();
 
             // wait for 2 seconds before sending next message
             Thread.sleep(2000);
         }
         
-//        byte[] message = "Crunchify".getBytes();
-//        ByteBuffer buffer = ByteBuffer.wrap(message);
-//        crunchifyClient.write(buffer);
         crunchifyClient.close();
     }
 
